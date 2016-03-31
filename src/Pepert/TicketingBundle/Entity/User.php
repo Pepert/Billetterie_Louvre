a5f174dfb2153a3500c73a5d1940751422a531db
Pepert <playpero@hotmail.com>
2016-03-31 17:28:48 +0200

    Création du TicketingBundle (bundle de gestion des tickets) et première mise en place des entités User et Ticket.

---
+++ src/Pepert/TicketingBundle/Entity/User.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace Pepert\TicketingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="Pepert\TicketingBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="visit_day", type="date")
+     */
+    private $visitDay;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ticket_type", type="string", length=255)
+     */
+    private $ticketType;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="ticket_number", type="integer")
+     */
+    private $ticketNumber;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set visitDay
+     *
+     * @param \DateTime $visitDay
+     *
+     * @return User
+     */
+    public function setVisitDay($visitDay)
+    {
+        $this->visitDay = $visitDay;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDay
+     *
+     * @return \DateTime
+     */
+    public function getVisitDay()
+    {
+        return $this->visitDay;
+    }
+
+    /**
+     * Set ticketType
+     *
+     * @param string $ticketType
+     *
+     * @return User
+     */
+    public function setTicketType($ticketType)
+    {
+        $this->ticketType = $ticketType;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketType
+     *
+     * @return string
+     */
+    public function getTicketType()
+    {
+        return $this->ticketType;
+    }
+
+    /**
+     * Set ticketNumber
+     *
+     * @param integer $ticketNumber
+     *
+     * @return User
+     */
+    public function setTicketNumber($ticketNumber)
+    {
+        $this->ticketNumber = $ticketNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get ticketNumber
+     *
+     * @return int
+     */
+    public function getTicketNumber()
+    {
+        return $this->ticketNumber;
+    }
+}
+
---
+++ src/Pepert/TicketingBundle/PepertTicketingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pepert\TicketingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PepertTicketingBundle extends Bundle
+{
+}
